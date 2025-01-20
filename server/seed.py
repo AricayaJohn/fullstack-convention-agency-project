6052daf662f5ca4d4a56d9ea4be2bbfa2e275330
John <jaricaya@gmail.com>
2025-01-20 23:20:58 +0800

    added concention_area seed

--- server/seed.py
+++ server/seed.py
@@ -2,6 +2,15 @@
 from app import app
 from models import db, Convention, ConventionArea, Attendee
 
+class clear_data():
+    Convention.query.delete()
+    ConventionArea.query.delete()
+    Attendee.query.delete()
+    db.session.commit()
+
+def add_convention_areas():
+    area1 = ConventionArea(location_name='Los Angeles Convention', venue='Staples Center')
+    area2 = ConventionArea(location_name='San Francisco Convention', venue='Oracle Arena')
 
 if __name__ == '__main__':
     fake = Faker()
