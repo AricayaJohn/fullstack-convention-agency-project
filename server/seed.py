442f6455756bf18d4e802f1d40b85c008197bf88
John <jaricaya@gmail.com>
2025-01-20 23:24:42 +0800

    added attendees seed

--- server/seed.py
+++ server/seed.py
@@ -12,6 +12,17 @@ def add_convention_areas():
     area1 = ConventionArea(location_name='Los Angeles Convention', venue='Staples Center')
     area2 = ConventionArea(location_name='San Francisco Convention', venue='Oracle Arena')
 
+    db.session.add_all([area1, area2])
+    db.session.commit()
+
+def add_attendees():
+    attendee1 = Attendee(name='John Smith', profession='Software Engineer')
+    attendee2 = Attendee(name='Jane Doe', profession='Restaurant Owner')
+
+    db.session.add_all([attendee1, attendee2])
+    db.session.commit()
+
+
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
