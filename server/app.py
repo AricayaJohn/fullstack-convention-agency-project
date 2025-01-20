c481d11d6dbcdb86e23a586ad16b6c1363562d7d
John <jaricaya@gmail.com>
2025-01-21 00:24:15 +0800

    added get and post for convention areas

--- server/app.py
+++ server/app.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 
-
-from flask import request
-from flask_restful import Resource
-
-from config import app, db, api
-# Add your model imports
-from models import db, Convention, ConventionArea, Attendee
-
+from flask import Flask, make_response, jsonify, request
+from flask_restful import Api, Resource
+from flask_migrate import flask_migrate
+from config import db, app
+from models import Convention, ConventionArea, Attendee
+import os
 
 # Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+class ConventionAreas(Resource):
+    def get(self):
+        areas = ConventionArea.query.all()
+        return [area.to_dct(only=('id', 'location_name', 'venue')) for area in areas]
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_area = ConventionArea(
+                location_name = data['location_name'],
+                venue=data['venue']
+            )
+            db.session.add(new_area)
+            db.session.commit()
+            return make_response(new_area.to_dict(), 201)
+        except ValueError:
+            return make_response({'errors': ['validataion errors']}, 400)
+
+
 
 
 if __name__ == '__main__':
