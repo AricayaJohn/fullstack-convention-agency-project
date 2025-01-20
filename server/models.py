1be6650b26985d619950197994f83d81d2b71a98
John <jaricaya@gmail.com>
2025-01-20 23:00:45 +0800

    added models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,56 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class ConventionArea(db.Model, SerializerMixin):
+    __tablename__ = 'convention_areas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location_name = db.Column(db.String, nullable=False)
+    venue = db.Column(db.String, nullable=False)
+
+    conventions = db.relationship('Convention', backref='convention_area', cascade="all, delete-orphan")
+
+    serialize_rules = ('-conventions.convention_area',)
+
+class Attendee(db.Model, SerializerMixin):
+    __trablename__ = 'attendees'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    profession = db.Column(db.String, nullable=False)
+
+    serialize_rules = ('-conventions.attendees',)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Attendee name cannot be empty')
+        return name
+
+    @validates('profession')
+    def validate_prodession(self, key, profession):
+        if not prodession:
+            raise ValueError('Profession cannot be empty')
+        return profession
+
+class Convention(db.Model, SerializerMixin):
+    __tablename__ = 'conventions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    convention_name = db.Column(db.String, nullable=False)
+    days = db.Column(db.Integer, nullable=False)
+
+    convention_area_id = db.Column(db.Integer, db.ForeignKey('convention_areas.id'), nuullable=False)
+    attendee_id = db.Column(db.Integer, db.ForeignKey('attendees.id'), nullable=False)
+
+    serialize_rules = ('-convention_area.conventions', '-attendees.conventions')
+
+    @validates('attendee_id')
+    def validate_attendee_id(self, key, attendee_id):
+        if not isInstance(attendee_id, int):
+            raise ValueError("attendees id must have an integer value")
+
+    @validates('convention_area_id')
+    def validate_convention_areas_id(self, key, convention_area_id):
+        if not isInstance(convention_area_id, int):
+            raide ValueError('convention area id must have an integer value')
