689e12d4550d0387f5f00131af579fa1a4c4036d
John <jaricaya@gmail.com>
2025-01-20 23:45:59 +0800

    added tables to database

--- server/app.py
+++ server/app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-# Standard library imports
 
-# Remote library imports
 from flask import request
 from flask_restful import Resource
 
-# Local imports
 from config import app, db, api
 # Add your model imports
+from models import db, Convention, ConventionArea, Attendee
 
 
 # Views go here!
