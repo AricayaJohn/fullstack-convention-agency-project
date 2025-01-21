efab1e659ad3f7fbbd893096cf759fe36653d0f4
John <jaricaya@gmail.com>
2025-01-21 21:33:50 +0800

    added convention page

--- client/src/components/ConventionsPage.js
+++ client/src/components/ConventionsPage.js
@@ -1,2 +1,39 @@
 import React, {useEffect, useState} from "react";
 import {useParams, Link} from "react-router-dom";
+import ConventionCard from "./ConventionCard";
+
+function ConventionPage() {
+    const [{ data: convention, error, status }, setConvention] = useState({
+        data: null,
+        error: null,
+        status: "pending",
+    });
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`/conventions/${id}`).then((response) => {
+            if(response.ok) {
+                response.json().then((err) => 
+                setConvention({data: null, error: err.error, status: "rejected"})
+            );
+            }
+        });
+    }, [id]);
+
+    if (status === "pending") return <h2>Loading...</h2>
+    if (status === "rejected") return <h2>Error: {error}</h2>
+
+    return (
+        <div>
+            <h1>Convention</h1>
+            {convention ? (
+                <ConventionCard key={convention.id} convention={convention} />
+            ) : (
+                <p>Convention not found</p>
+            )}
+            <Link to="/"> Back to Home </Link>
+        </div>
+    );
+};
+
+export default ConventionPage
