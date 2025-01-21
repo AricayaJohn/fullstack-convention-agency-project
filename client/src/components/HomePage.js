673d2803edb452e9d051f237c6be389b10531244
John <jaricaya@gmail.com>
2025-01-21 09:02:12 +0800

    added homepage

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState} from "react";
+import ConventionAreaCard from "./ConventionAreaCard";
+import { Link } from "react-router-dom";
+
+function HomePage(){
+    const [conventionAreas, setConventionAreas] = useState([])
+
+    useEffect(() => {
+        fetch('/convention_areas')
+        .then((response) => response.json())
+        .then((data) => setConventionAreas(data))
+    }, []);
+
+    return (
+        <div>
+            <h1>Convention Areas</h1>
+            <Link to="/add-convention-area">
+            Add Convention Area
+            </Link>
+            {conventionAreas.map((area) => (
+                <ConventionAreaCard key={area.id} area={area} />
+            ))}
+        </div>
+    );
+}
+
+export default HomePage;
