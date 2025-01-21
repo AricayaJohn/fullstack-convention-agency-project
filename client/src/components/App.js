9581e5babb36f059357ea8a7e156f06ac39e2143
John <jaricaya@gmail.com>
2025-01-21 09:11:00 +0800

    Home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import HomePage from "./HomePage";
 import ConventionAreaForm from "./ConventionAreaForm";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <main>
+      <h1>
+        <Link to="/">Convention Agency</Link>
+      </h1>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/add-convention-area">
+          <ConventionAreaForm />
+        </Route>
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ConventionAreaCard from "./ConventionAreaCard";
 import { Link } from "react-router-dom";
 
