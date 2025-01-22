1c600a7bee7c6cac5494195aa618fa43bc253aeb
John <jaricaya@gmail.com>
2025-01-23 04:26:55 +0800

    fetch for attendee form

--- client/src/components/AddAttendeeForm.js
+++ client/src/components/AddAttendeeForm.js
@@ -4,5 +4,25 @@ function AddAttendeeFrom({conventionId, onAddAttendee}) {
     const [name, setName] = useState("");
     const [profession, setProfession] = useState("");
 
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('/attendees', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                profession,
+                conventionId: parseInt(conventionId)
+            }),
+        })
+        .then(response => response.json())
+        .then(newAttendee => {
+            onAddAttendee(newAttendee);
+            setName("");
+            setProfession("");
+        })
+        .catch(error => console.error('Error:', error));
+    };
 }
