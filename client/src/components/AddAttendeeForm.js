5682ce55fb8ef71a4a6fc78b9bd875aa9c94c69a
John <jaricaya@gmail.com>
2025-01-23 04:22:00 +0800

    added attendees form state

---
+++ client/src/components/AddAttendeeForm.js
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+
+function AddAttendeeFrom({conventionId, onAddAttendee}) {
+    const [name, setName] = useState("");
+    const [profession, setProfession] = useState("");
+
+    }
+}
